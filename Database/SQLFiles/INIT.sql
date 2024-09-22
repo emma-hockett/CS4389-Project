5428ca6258ba92406bd2af0b2ac478cc8b79886e
ArielCOng <92648240+ArielCOng@users.noreply.github.com>
2024-09-22 13:18:19 -0500

    Add files via upload

---
+++ Database/SQLFiles/INIT.sql
@@ -0,0 +1,3 @@
+SOURCE C:/Users/sprin/OneDrive/Documents/GitHub/CodeBuster/Database/SQLFiles/create.sql;
+USE spotify;
+SOURCE C:/Users/sprin/OneDrive/Documents/GitHub/CodeBuster/Database/SQLFiles/load.sql;
