c20b52cfea41b2d4efddd77f461dce39effd7312
kennedy-onyia <145404406+kennedy-onyia@users.noreply.github.com>
2024-10-08 21:15:02 -0500

    Updated SQL commands in create.sql to ensure compatibility with PostgreSQL, including syntax adjustments and datatype refinements.

--- Database/SQLFiles/create.sql
+++ Database/SQLFiles/create.sql
@@ -1,50 +1,45 @@
 DROP DATABASE IF EXISTS cubebuster;
 CREATE DATABASE cubebuster;
-USE cubebuster;
 
 CREATE TABLE Movie (
-        MovieID int NOT NULL AUTO_INCREMENT,
+        MovieID serial PRIMARY KEY,
         Title varchar(40) NOT NULL,
         movieYear int NOT NULL,
         Genre varchar(15) NOT NULL,
         Director varchar(20) NOT NULL,
         Price Decimal(3,2) NOT NULL,
         Rating varchar(5) NOT NULL,
-        Duration int NOT NULL,
-        PRIMARY KEY(MovieID)
+        Duration int NOT NULL
 );
 
 CREATE TABLE Employee (
-        EmployeeID int NOT NULL AUTO_INCREMENT,
+        EmployeeID serial PRIMARY KEY,
         empName varchar(20) NOT NULL,
         Address varchar(30) NOT NULL,
         SSN char(9) NOT NULL UNIQUE,
         Email varchar(25) NOT NULL UNIQUE, 
         Phone char(10) NOT NULL,
-        empPassword varchar(20) NOT NULL,
-        PRIMARY KEY(EmployeeID)
+        empPassword varchar(20) NOT NULL
 );
 
 
 CREATE TABLE Customer (
-        cID int NOT NULL AUTO_INCREMENT,
+        cID serial PRIMARY KEY,
         username varchar (20) NOT NULL,
         Address varchar(30),
         CCNum char(16),
         Bday date NOT NULL,
         Email varchar(25) NOT NULL UNIQUE,
-        userpswd varchar(20) NOT NULL,
-		PRIMARY KEY(cID)
+        userpswd varchar(20) NOT NULL
 );
 
 CREATE TABLE Store (
-        StoreID int NOT NULL AUTO_INCREMENT,
+        StoreID serial PRIMARY KEY,
         BuildingNum int NOT NULL,
         Street varchar(20) NOT NULL,
         City varchar(20) NOT NULL,
         Zipcode int NOT NULL, 
-        State varchar(2) NOT NULL, 
-		PRIMARY KEY(StoreID)
+        State varchar(2) NOT NULL
 );
 
 CREATE TABLE Request (
@@ -54,7 +49,7 @@ CREATE TABLE Request (
         Place int NOT NULL,
         reqStatus char(8) NOT NULL,
         DueDate date, 
-        Balance float NOT NULL,
+        Balance numeric(6,2) NOT NULL,
         PRIMARY KEY (MovieID, UserID, StoreID),
         FOREIGN KEY (MovieID) REFERENCES Movie(MovieID),
         FOREIGN KEY (UserID) REFERENCES Customer(cID),
@@ -72,7 +67,7 @@ CREATE TABLE WorksAt (
 CREATE TABLE InStock (
         MovieID int NOT NULL,
         StoreID int NOT NULL,
-        IsAvailable bool NOT NULL,
+        IsAvailable boolean NOT NULL,
         Quantity int NOT NULL,
         PRIMARY KEY (MovieID, StoreID),
         FOREIGN KEY (MovieID) REFERENCES Movie(MovieID),
