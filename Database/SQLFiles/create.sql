8870853ca9ea9d9bdb19daa26e87b2b9fe22ba9b
Emma Hockett <146998972+emma-hockett@users.noreply.github.com>
2024-10-14 14:07:19 -0500

    MySQL, small changes

--- Database/SQLFiles/create.sql
+++ Database/SQLFiles/create.sql
@@ -1,45 +1,51 @@
 DROP DATABASE IF EXISTS cubebuster;
 CREATE DATABASE cubebuster;
+USE cubebuster;
 
 CREATE TABLE Movie (
-        MovieID serial PRIMARY KEY,
+        MovieID int NOT NULL AUTO_INCREMENT,
         Title varchar(40) NOT NULL,
         movieYear int NOT NULL,
         Genre varchar(15) NOT NULL,
         Director varchar(20) NOT NULL,
         Price Decimal(3,2) NOT NULL,
         Rating varchar(5) NOT NULL,
-        Duration int NOT NULL
+        Duration int NOT NULL,
+        ImageAddress varchar(40),
+        PRIMARY KEY(MovieID)
 );
 
 CREATE TABLE Employee (
-        EmployeeID serial PRIMARY KEY,
+        EmployeeID int NOT NULL AUTO_INCREMENT,
         empName varchar(20) NOT NULL,
         Address varchar(30) NOT NULL,
         SSN char(9) NOT NULL UNIQUE,
         Email varchar(25) NOT NULL UNIQUE, 
         Phone char(10) NOT NULL,
-        empPassword varchar(20) NOT NULL
+        empPassword varchar(20) NOT NULL,
+        PRIMARY KEY(EmployeeID)
 );
 
 
 CREATE TABLE Customer (
-        cID serial PRIMARY KEY,
+        cID int NOT NULL AUTO_INCREMENT,
         username varchar (20) NOT NULL,
         Address varchar(30),
         CCNum char(16),
         Bday date NOT NULL,
         Email varchar(25) NOT NULL UNIQUE,
-        userpswd varchar(20) NOT NULL
+        userpswd varchar(20) NOT NULL,
+		PRIMARY KEY(cID)
 );
 
 CREATE TABLE Store (
-        StoreID serial PRIMARY KEY,
+        StoreID int NOT NULL AUTO_INCREMENT,
         BuildingNum int NOT NULL,
         Street varchar(20) NOT NULL,
         City varchar(20) NOT NULL,
         Zipcode int NOT NULL, 
-        State varchar(2) NOT NULL
+        State varchar(2) NOT NULL, 
+		PRIMARY KEY(StoreID)
 );
 
 CREATE TABLE Request (
@@ -49,7 +55,8 @@ CREATE TABLE Request (
         Place int NOT NULL,
         reqStatus char(8) NOT NULL,
         DueDate date, 
-        Balance numeric(6,2) NOT NULL,
+        Balance float NOT NULL,
+        ReqTime TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
         PRIMARY KEY (MovieID, UserID, StoreID),
         FOREIGN KEY (MovieID) REFERENCES Movie(MovieID),
         FOREIGN KEY (UserID) REFERENCES Customer(cID),
@@ -67,7 +74,7 @@ CREATE TABLE WorksAt (
 CREATE TABLE InStock (
         MovieID int NOT NULL,
         StoreID int NOT NULL,
-        IsAvailable boolean NOT NULL,
+        IsAvailable bool NOT NULL,
         Quantity int NOT NULL,
         PRIMARY KEY (MovieID, StoreID),
         FOREIGN KEY (MovieID) REFERENCES Movie(MovieID),
