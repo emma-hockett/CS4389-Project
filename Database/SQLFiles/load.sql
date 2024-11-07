6e11fc92bd2a9bef511393369b6b776bed107293
Nicole Iwuala <113850453+nikkinikki247@users.noreply.github.com>
2024-11-07 15:24:59 -0800

    Update load.sql
    Took out PGRating because yall didn't put it in create.sql

--- Database/SQLFiles/load.sql
+++ Database/SQLFiles/load.sql
@@ -1,23 +1,23 @@
 USE cubebuster;
 
-INSERT INTO Movie (MovieID, Title, movieYear, Genre, Director, Price, PGRating, Rating, Duration, ImageAddress)
+INSERT INTO Movie (MovieID, Title, movieYear, Genre, Director, Price, Rating, Duration, ImageAddress)
 VALUES
-('tt0111161', 'The Shawshank Redemption', 1994, 'drama', 'Frank Darabont', 14.99, 'R', 9.3, 142, 'https://tinyurl.com/2p9jtrpp'),
-('tt0068646', 'The Godfather', 1972, 'crime', 'Francis Ford Coppola', 16.99, 'R', 9.2, 175, 'https://tinyurl.com/3yypb7c4'),
-('tt0468569', 'The Dark Knight', 2008, 'action', 'Christopher Nolan', 13.99, 'PG-13', 9.0, 152, 'https://tinyurl.com/2ea8bb6h'),
-('tt0137523', 'Fight Club', 1999, 'drama', 'David Fincher', 12.99, 'R', 8.8, 139, 'https://tinyurl.com/yt7u37wf'),
-('tt0109830', 'Forrest Gump', 1994, 'comedy', 'Robert Zemeckis', 11.99, 'PG-13', 8.8, 142, 'https://tinyurl.com/5n6vffzy'),
-('tt0120737', 'The Lord of the Rings: The Fellowship of the Ring', 2001, 'fantasy', 'Peter Jackson', 15.99, 'PG-13', 8.8, 178, 'https://tinyurl.com/5ha7s4fu'),
-('tt0080684', 'Star Wars: Episode V - The Empire Strikes Back', 1980, 'sci-fi', 'Irvin Kershner', 14.49, 'PG', 8.7, 124, 'https://tinyurl.com/3cbw72dj'),
-('tt1853728', 'Django Unchained', 2012, 'western', 'Quentin Tarantino', 13.49, 'R', 8.4, 165, 'https://tinyurl.com/mvwym52h'),
-('tt0167260', 'The Lord of the Rings: The Return of the King', 2003, 'fantasy', 'Peter Jackson', 15.49, 'PG-13', 9.0, 201, 'https://tinyurl.com/mwczhtra'),
-('tt7286456', 'Joker', 2019, 'crime', 'Todd Phillips', 17.99, 'R', 8.4, 122, 'https://tinyurl.com/54rv6ade'),
-('tt1375666', 'Inception', 2010, 'sci-fi', 'Christopher Nolan', 14.99, 'PG-13', 8.8, 148, 'https://tinyurl.com/yc37m33j'),
-('tt0133093', 'The Matrix', 1999, 'sci-fi', 'Lana Wachowski, Lilly Wachowski', 12.49, 'R', 8.7, 136, 'https://tinyurl.com/5fcytauw'),
-('tt0816692', 'Interstellar', 2014, 'sci-fi', 'Christopher Nolan', 15.99, 'PG-13', 8.6, 169, 'https://tinyurl.com/233573db'),
-('tt3783958', 'La La Land', 2016, 'comedy', 'Damian Chazelle', 10.99, 'PG-13', 8.0, 128, 'https://tinyurl.com/a3b7kj5v'),
-('tt0114369', 'Se7en', 1995, 'crime', 'David Fincher', 13.99, 'R', 8.6, 127, 'https://tinyurl.com/ybn6zwxb'),
-('tt0110413', 'Léon: The Professional', 1994, 'romance', 'Luc Besson', 9.99, 'R', 8.5, 110, 'https://tinyurl.com/yk4yh76y'),
-('tt0120586', 'American History X', 1998, 'action', 'Tony Kaye', 11.99, 'R', 8.5, 119, 'https://tinyurl.com/yj4r4xhd'),
-('tt0088763', 'Back to the Future', 1985, 'adventure', 'Robert Zemeckis', 10.49, 'PG', 8.5, 116, 'https://tinyurl.com/3kt3a4vp');
+('tt0111161', 'The Shawshank Redemption', 1994, 'drama', 'Frank Darabont', 14.99, 9.3, 142, 'https://tinyurl.com/2p9jtrpp'),
+('tt0068646', 'The Godfather', 1972, 'crime', 'Francis Ford Coppola', 16.99, 9.2, 175, 'https://tinyurl.com/3yypb7c4'),
+('tt0468569', 'The Dark Knight', 2008, 'action', 'Christopher Nolan', 13.99, 9.0, 152, 'https://tinyurl.com/2ea8bb6h'),
+('tt0137523', 'Fight Club', 1999, 'drama', 'David Fincher', 12.99, 8.8, 139, 'https://tinyurl.com/yt7u37wf'),
+('tt0109830', 'Forrest Gump', 1994, 'comedy', 'Robert Zemeckis', 11.99, 8.8, 142, 'https://tinyurl.com/5n6vffzy'),
+('tt0120737', 'The Lord of the Rings: The Fellowship of the Ring', 2001, 'fantasy', 'Peter Jackson', 15.99, 8.8, 178, 'https://tinyurl.com/5ha7s4fu'),
+('tt0080684', 'Star Wars: Episode V - The Empire Strikes Back', 1980, 'sci-fi', 'Irvin Kershner', 14.49, 8.7, 124, 'https://tinyurl.com/3cbw72dj'),
+('tt1853728', 'Django Unchained', 2012, 'western', 'Quentin Tarantino', 13.49, 8.4, 165, 'https://tinyurl.com/mvwym52h'),
+('tt0167260', 'The Lord of the Rings: The Return of the King', 2003, 'fantasy', 'Peter Jackson', 15.49, 9.0, 201, 'https://tinyurl.com/mwczhtra'),
+('tt7286456', 'Joker', 2019, 'crime', 'Todd Phillips', 17.99, 8.4, 122, 'https://tinyurl.com/54rv6ade'),
+('tt1375666', 'Inception', 2010, 'sci-fi', 'Christopher Nolan', 14.99, 8.8, 148, 'https://tinyurl.com/yc37m33j'),
+('tt0133093', 'The Matrix', 1999, 'sci-fi', 'Lana Wachowski, Lilly Wachowski', 12.49, 8.7, 136, 'https://tinyurl.com/5fcytauw'),
+('tt0816692', 'Interstellar', 2014, 'sci-fi', 'Christopher Nolan', 15.99, 8.6, 169, 'https://tinyurl.com/233573db'),
+('tt3783958', 'La La Land', 2016, 'comedy', 'Damian Chazelle', 10.99, 8.0, 128, 'https://tinyurl.com/a3b7kj5v'),
+('tt0114369', 'Se7en', 1995, 'crime', 'David Fincher', 13.99, 8.6, 127, 'https://tinyurl.com/ybn6zwxb'),
+('tt0110413', 'Léon: The Professional', 1994, 'romance', 'Luc Besson', 9.99,  8.5, 110, 'https://tinyurl.com/yk4yh76y'),
+('tt0120586', 'American History X', 1998, 'action', 'Tony Kaye', 11.99, 8.5, 119, 'https://tinyurl.com/yj4r4xhd'),
+('tt0088763', 'Back to the Future', 1985, 'adventure', 'Robert Zemeckis', 10.49, 8.5, 116, 'https://tinyurl.com/3kt3a4vp');
 
